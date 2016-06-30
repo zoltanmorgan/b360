63c45e864c4eefc879876bc33aaabd7a35434c80
zoltanmorgan <morganzoltan@gmail.com>
2016-06-30 15:33:57 +0300

    Main.js için taban oluşturuldu.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,52 @@
-/**
- * Created by Mergen on 28.06.2016.
- */
+/* Created by Mergen*/
+var scene, camera, renderer;
+var boxGeometry, boxMesh, boxMaterial;
+
+init();
+
+function init() {
+    scene = new THREE.Scene();
+    initMaterials();
+    initMeshes();
+    initCamera();
+    initLights();
+    initRenderer();
+}
+
+function initMaterials() {
+    boxMaterial = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+}
+
+function initMeshes() {
+    boxGeometry = new THREE.BoxGeometry( 1, 1, 1 );
+    boxMesh = new THREE.Mesh( boxGeometry, boxMaterial );
+    scene.add( boxMesh );
+}
+
+function initCamera() {
+    camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
+    camera.position.z = 5;
+}
+
+function initLights() {
+    
+}
+
+function initRenderer() {
+    renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+}
+
+function animation() {
+    boxMesh.rotation.x += 0.02;
+    boxMesh.rotation.y += 0.02;
+}
+
+var render = function () {
+    requestAnimationFrame( render );
+    animation();
+    renderer.render(scene, camera);
+};
+
+render();
