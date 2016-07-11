3951b9783cad07000c2c7b86f35fe9db2120a922
zoltanmorgan <morganzoltan@gmail.com>
2016-07-11 22:51:19 +0300

    Footer added.

--- js/main.js
+++ js/main.js
@@ -5,9 +5,16 @@
 
     document.body.style.width = windowWidth+"px";
     document.getElementById("header").style.width = windowWidth+"px";
+    
     document.getElementById("tour").style.height = (windowHeight)+"px";
     document.getElementById("tour").style.width = windowWidth+"px";
+    
     document.getElementById("headercontent").style.left = ((windowWidth-1200)/2)+"px";
+
+    document.getElementById("footer").style.top = (windowHeight-100)+"px";
+    document.getElementById("footer").style.width = windowWidth+"px";
+    document.getElementById("footercontent").style.left = ((windowWidth-800)/2)+"px";
+    
     document.getElementById("tourFrame").style.height = windowHeight+"px";
     document.getElementById("tourFrame").style.width = windowWidth+"px";
 
