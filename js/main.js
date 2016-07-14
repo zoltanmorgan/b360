c41945e52d9d142ccb77a4f5bf24c500c7ee573a
zoltanmorgan <morganzoltan@gmail.com>
2016-07-14 15:43:29 +0300

    Animations added.

--- js/main.js
+++ js/main.js
@@ -1,36 +1,48 @@
     var windowWidth, windowHeight;
-    var leftcontHeight, leftcontWidth;
-    windowWidth = window.innerWidth;
-    windowHeight = window.innerHeight;
-    leftcontHeight = document.getElementById("leftcontent").offsetHeight;
-    leftcontWidth = document.getElementById("leftcontent").offsetWidth;
+    var headerHeight, tourHeight;
+
+    window.addEventListener("resize", setPositions);
 
     setPositions()
 
     function setPositions() {
+
+        windowWidth = window.innerWidth;
+        windowHeight = window.innerHeight;
+        headerHeight = document.getElementById("headercontent").offsetHeight;
+
         document.body.style.width = windowWidth+"px";
 
-        document.getElementById("leftbar").style.height = windowHeight+"px";
-        document.getElementById("leftbar").style.width = leftcontWidth+80+"px";
-        document.getElementById("leftbar").style.top =  0+"px";
-        document.getElementById("leftbar").style.left = 0+"px";
-
-        document.getElementById("leftcontent").style.top = ((windowHeight-leftcontHeight)/3)+"px";
-        document.getElementById("leftcontent").style.left = 40+"px";
-
-
-        document.getElementById("tour").style.height = (windowHeight)+"px";
+        document.getElementById("tour").style.height = (windowHeight-headerHeight)+"px";
         document.getElementById("tour").style.width = windowWidth+"px";
 
-        document.getElementById("headercontent").style.left = ((windowWidth-1200)/2)+"px";
+        if (windowWidth  < 900) {
+            $( "#projects" ).width("600px");
+            document.getElementById("projects").style.left = ((windowWidth-600)/2)+"px";
 
-        document.getElementById("footer").style.top = (windowHeight-100)+"px";
-        document.getElementById("footer").style.width = windowWidth+"px";
-        document.getElementById("footercontent").style.left = ((windowWidth-800)/2)+"px";
+            $( "#headercontent" ).width("600px");
+            document.getElementById("headercontent").style.left = ((windowWidth-600)/2)+"px";
+        }
 
-        document.getElementById("tourFrame").style.height = windowHeight+"px";
-        document.getElementById("tourFrame").style.width = windowWidth+"px";
+        else {
+            $( "#projects" ).width("900px");
+            document.getElementById("projects").style.left = ((windowWidth-900)/2)+"px";
+
+            $( "#headercontent" ).width("900px");
+            document.getElementById("headercontent").style.left = ((windowWidth-900)/2)+"px";
+        }
+
     }
 
 
 
+    $( ".proj" )
+        .mouseover(function() {
+            // i += 1;
+
+            $(this).children(".projtnt").stop().animate({height: '100px', opacity: '0.8', top: '200px'}, "fast");
+        })
+        .mouseout(function() {
+        //    $( this ).text( "mouse out " );
+            $(this).children(".projtnt").stop().animate({height: '50px', opacity: '0', top: '250px'}, "fast");
+        });
